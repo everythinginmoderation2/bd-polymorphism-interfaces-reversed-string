e157fef9c9a01e97953439e4b48640bb996ac292
Prince Slater <everythingindmoderation2@gmail.com>
2024-09-15 16:14:19 -0400

    ReversedString implements charSequence

--- src/com/amazon/ata/interfaces/ReversedString.java
+++ src/com/amazon/ata/interfaces/ReversedString.java
@@ -5,15 +5,38 @@ package com.amazon.ata.interfaces;
  * <p>
  * Change this class definition to implement CharSequence.
  */
-public class ReversedString {
-
-
+public class ReversedString implements CharSequence {
+    private String chars;
     /**
      * Implement this constructor during the interfaces prework.
      *
      * @param chars The String of chars to be reversed
      */
     public ReversedString(final String chars) {
+        this.chars = chars;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return chars.charAt(chars.length() - 1 - index);
+    }
+
+    @Override
+    public int length() {
+        return chars.length();
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        String reversedSub = chars.substring(
+                chars.length()-end,
+                chars.length()-start);
+        return new ReversedString(reversedSub);
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(chars).reverse().toString();
     }
 
 }
